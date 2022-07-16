a8710a5b93246c0b545e93543bd050c3b23ee281
aseel-sm <aseelsm0027@gmail.com>
2022-07-16 17:52:18 +0530

    completed project

---
+++ static/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $("#loading").hide();
+
+  $("#predict-btn").click(() => {
+    $("#loading").show();
+    let uploadedImage = new FormData($("#upload-form")[0]);
+
+    $.ajax({
+      type: "POST",
+      url: "/predict",
+      data: uploadedImage,
+      contentType: false,
+      cache: false,
+      processData: false,
+      async: true,
+      success: function (data) {
+        $("#loading").hide();
+
+        content = "<button class='btn btn-danger'>Error</button>";
+        if (data["status"] == "success") {
+          content =
+            "<p>Bird: <strong>" +
+            data["bird"] +
+            "</strong></p><p>Conservation Status: <strong>" +
+            data["Cstatus"] +
+            "</strong></p><p>Bird Common: <strong>" +
+            data["birdCommon"] +
+            "</strong></p><p>Bird Category: <strong>" +
+            data["birdCat"] +
+            "</strong></p><p>Bird Period: <strong>" +
+            data["birdSeason"] +
+            "</strong></p>";
+
+          $("#imageWidget").attr("src", data["image"]);
+          $("#imageWidget").show();
+        }
+
+        $("#result").html(content);
+        console.log(data);
+      },
+    });
+  });
+});
